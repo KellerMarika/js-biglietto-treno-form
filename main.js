dba9cc6fa53cf8c3a1454a32fd7396f00d7efde1
KellerMarika <marikakeller92@gmail.com>
2022-10-30 01:01:13 +0200

    prima function

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const btnCancel = document.getElementById("cancel");
 /* INIZIO FUNZIONE ****************************************************/
 //evento:click
 btnTiketPrinter.addEventListener("click", function () {
-    
+
     /**** NOME UTENTE  *******************/
     // al btn-click raccolgo il valore dell'input e lo assegno come valore della variabile userId
     userId = document.getElementById("user-id").value;
@@ -42,7 +42,7 @@ btnTiketPrinter.addEventListener("click", function () {
     /*** USER KM ************************/
     //  al btn-click raccolgo il valore dell'input e lo assegno come valore della variabile userTravelWidth
     userTravelWidth = document.getElementById("user-travel-width").value;
-    console.log(`userTravelWidth = ${userTravelWidth} ${typeof userTravelWidth}`);
+    console.log(`userTravelWidth = ${userTravelWidth} ${typeof userTravelWidth}`);//controllo valore
 
     //CONTROLLI SUI KM::
 
@@ -58,27 +58,54 @@ btnTiketPrinter.addEventListener("click", function () {
         console.log("n km= ", userTravelWidth, typeof userTravelWidth);
     }
 
-//USER AGE al btn-click raccolgo il valore dell'input e lo assegno come valore della variabile userAge
-userAge = document.getElementById("user-age").value;
-console.log(`userAge = ${userAge}`);
-
-if (userAge=="a"){
-    console.log(`userAge value = ${userAge}  MINORENNE`);
-} else if(userAge=="c"){
-    console.log(`userAge value = ${userAge}  OVER65`);
-}else{
-    console.log(`userAge value = ${userAge}  MAGGIORENNE`);
-}
-
-    //PREZZO...................................................
+    /*** USER AGE  ************************/
+    // al btn-click raccolgo il valore dell'input e lo assegno come valore della variabile userAge
+    userAge = document.getElementById("user-age").value;
+    console.log(`userAge = ${userAge}`);//controllo valore
 
 
+    /*** PREZZO ************************/
+    /* calcolo prezzo base €/km */
+    const price = (userTravelWidth * .21);
+    console.log(price);
+
+    /* dichiaro priceList ma assegno un valore diverso a seconda della fascia d'età dentro le if/else */
+    let priceList
+
+    /* tiket  offer= vi stampo(innerHTML) un valore differente in base alla fascia d'età dentro le if/else */
+    const tiketUserOffer = document.getElementById("tiket-user-offer");
+
+    /* condizioni  
+    se utente minorenne : sconto 20% ; se utente over65: sconto 40%   
+    calc: x:price=%:100  
+    invece di sottrarre la percentuale ottenuta all'intero, moltiplico direttamente per la percentuale complementare allo sconto.*/
+
+    //minorenni
+    if (userAge == "disc20-100") {
+
+        priceList = (price * .8);
+        tiketUserOffer.innerHTML = "scono 20% Minorenni";
+        //over65
+    } else if (userAge == "disc40-100") {
+
+        priceList = (price * .6);
+        tiketUserOffer.innerHTML = "scono 40% Over65";
+        //tutti gli altri
+    } else {
+        priceList = price;
+        tiketUserOffer.innerHTML = "prezzo intero adulti";
+    }
 
 
-    //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //a tutti aggiungo l'euro e tolgo i decimali in eccesso
+    //per fatrlo ho bisogno che la voariabile uscente abbia lo stesso nome per tutti gli if
+    userPrice = `${(priceList.toFixed(2))} €`;
+    console.log(userPrice);
 
 
-
+    const tiketUserPrice = document.getElementById("tiket-user-price");
+    tiketUserPrice.innerHTML=`${(priceList.toFixed(2))} €`
+   
 
 
     /**** HTML insert value + PRINTER *************************************/
